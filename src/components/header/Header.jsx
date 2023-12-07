da702710f20d235fceb55073efc7539982aabe06
Shaid Islam <shaid85@gmail.com>
2023-12-07 18:44:37 +0600

    Header adjust

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -41,7 +41,7 @@ useEffect(() => {
            
     }    
     return (
-        <header className="shadow sticky z-50 top-0 dark:bg-gray-800 text-white">
+        <header className=" bg-white shadow sticky z-50 top-0 dark:bg-gray-800 text-white">
             <nav className="border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
