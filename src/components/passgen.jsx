1bd50aec2a64b7c04d56c89f6cd4a98b168d0203
Shaid Islam <shaid85@gmail.com>
2023-12-07 18:55:30 +0600

    passgen adjust

--- src/components/passgen.jsx
+++ src/components/passgen.jsx
@@ -36,11 +36,11 @@ const PassGen = () => {
 
     return (
       <>
-        <div className="w-full max-w-md mx-auto shadow-lg rounded-lg px-4 py-2 my-20  text-orange-500  bg-gray-500">
+        <div className="w-full max-w-md mx-auto shadow-lg rounded-lg px-4 py-2 my-20  text-white  bg-gray-500">
            <h2 className="text-center text-white p-4">Password Generator</h2>
             <div className="flex shadow rounded-lg overflow-hidden mb-4">
               <input 
-              className="outline-none w-full py-1 px-3" readOnly
+              className="outline-none w-full py-1 px-3 text-black" readOnly
               placeholder="Password"
               type="text" 
               value={password} 
